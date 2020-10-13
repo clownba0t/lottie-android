df66eb7d46eb9691b70f63c595c5c028b3b75f8c
Daniel Carter <daniel.carter@paypay-corp.co.jp>
2020-10-13 17:51:46 +0900

    Configure app to allow easy debugging of lack of memory cache issue.

--- issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
@@ -2,15 +2,24 @@ package com.airbnb.lottie.issues
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import com.airbnb.lottie.issues.R
 import kotlinx.android.synthetic.main.activity_main.*
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Reproduce any issues here.
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.reload).setOnClickListener { load() }
+        load()
+    }
+
+    private fun load() {
+        findViewById<LottieAnimationView>(R.id.animation).setAnimationFromUrl("https://raw.githubusercontent.com/airbnb/lottie-ios/master/Example/lottie-swift/TestAnimations/PinJump.json")
     }
 }
